74b95d9970350a8c2bd335bc5be97b342fe4bb62
LouisBienz <louis.bienz@gmail.com>
2022-08-17 11:10:53 +0200

    require location permission, start construct for BLE scan when clicking on SCAN button

--- app/src/main/java/android/example/homescout/MainActivity.kt
+++ app/src/main/java/android/example/homescout/MainActivity.kt
@@ -1,11 +1,13 @@
 package android.example.homescout
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,18 +16,31 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import android.example.homescout.databinding.ActivityMainBinding
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 
 private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
 
 class MainActivity : AppCompatActivity() {
+
+    // PROPERTIES
 
     private lateinit var binding: ActivityMainBinding
 
     private val bluetoothAdapter: BluetoothAdapter by lazy {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothManager.adapter
     }
+
+    val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+
+    // OVERRIDES
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +61,12 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        binding.buttonScan.setOnClickListener{
+            startBLEScan()
+        }
+
     }
+
 
 
     override fun onResume() {
@@ -67,10 +87,68 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
             ENABLE_BLUETOOTH_REQUEST_CODE -> {
-                if (resultCode != Activity.RESULT_OK) {
+                if (resultCode != RESULT_OK) {
                     promptEnableBluetooth()
                 }
             }
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
+                    requestLocationPermission()
+                } else {
+                    startBLEScan()
+                }
+            }
+        }
+    }
+
+    // PRIVATE FUNCTIONS
+
+    private fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startBLEScan() {
+        if (!isLocationPermissionGranted) {
+            requestLocationPermission()
+        } else {
+            Snackbar.make(
+                this.findViewById(R.id.nav_host_fragment_activity_main),
+                "Location Permission Granted",
+                Snackbar.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (isLocationPermissionGranted) {
+            return
+        }
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Location Permission required")
+            .setMessage("Starting from Android M (6.0), the system requires apps to be granted " +
+                "location access in order to scan for BLE devices.")
+            .setPositiveButton("Ok") { dialog, which ->
+                requestPermission(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+            .show()
+    }
+
+    private fun Activity.requestPermission(permission: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    }
+
 }
