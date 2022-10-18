f56395ade17ada6923a1505e12b0f5dee16055bb
LouisBienz <louis.bienz@gmail.com>
2022-10-18 11:16:06 +0200

    refactor home to welcome fragment

--- app/src/main/java/android/example/homescout/ui/home/WelcomeFragment.kt
+++ app/src/main/java/android/example/homescout/ui/home/WelcomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import android.example.homescout.databinding.FragmentHomeBinding
+import android.example.homescout.databinding.FragmentWelcomeBinding
 
-class HomeFragment : Fragment() {
+class WelcomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentWelcomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(WelcomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.textWelcome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
