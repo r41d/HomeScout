d7f061be560610b6b2e925aca7be802713242389
LouisBienz <louis.bienz@gmail.com>
2022-10-18 11:17:36 +0200

    create scan fragment with viewModel

---
+++ app/src/main/java/android/example/homescout/ui/scan/ScanViewModel.kt
@@ -0,0 +1,13 @@
+package android.example.homescout.ui.scan
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ScanViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is scan Fragment"
+    }
+    val text: LiveData<String> = _text
+}
