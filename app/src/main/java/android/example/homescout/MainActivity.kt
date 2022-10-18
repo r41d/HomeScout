3fb7ebc354f1561b3e3eb0b10f22791cf0d630f3
LouisBienz <louis.bienz@gmail.com>
2022-10-18 18:01:37 +0200

    remove scanning from MainActivity.kt

--- app/src/main/java/android/example/homescout/MainActivity.kt
+++ app/src/main/java/android/example/homescout/MainActivity.kt
@@ -6,27 +6,33 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.example.homescout.databinding.ActivityMainBinding
+import android.example.homescout.models.DeviceFactory
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import android.example.homescout.databinding.ActivityMainBinding
-import android.example.homescout.models.DeviceFactory
-import android.util.Log
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlin.experimental.and
 
 
 private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
@@ -35,49 +41,11 @@ private const val LOCATION_PERMISSION_REQUEST_CODE = 2
 class MainActivity : AppCompatActivity() {
 
     // PROPERTIES
-
     private lateinit var binding: ActivityMainBinding
 
 
-    private val bluetoothAdapter: BluetoothAdapter by lazy {
-        val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
-    }
 
-
-    private val bleScanner by lazy {
-        bluetoothAdapter.bluetoothLeScanner
-    }
-
-    private val scanSettings = ScanSettings.Builder()
-        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-        .build()
-
-    private var isScanning = false
-        set(value) {
-            field = value
-            if (value) {
-                binding.buttonScan.text = "Stop"
-            } else {
-                binding.buttonScan.text = "scan"
-            }
-        }
-
-    private val scanResults = mutableListOf<ScanResult>()
-
-    private val scanResultAdapter: ScanResultAdapter by lazy {
-        ScanResultAdapter(scanResults) {
-            // TODO: Implement
-        }
-    }
-
-
-    val isLocationPermissionGranted
-        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-
-
-    // OVERRIDES
-
+    // LIFECYCLE FUNCTIONS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -86,163 +54,21 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_welcome,
+                R.id.navigation_notifications,
+                R.id.navigation_settings,
+                R.id.navigation_scan
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        binding.buttonScan.setOnClickListener{
-            if (isScanning) {
-                stopBleScan()
-            } else {
-                startBLEScan()
-            }
-        }
-        setupRecyclerView()
-
-    }
-
-
-    override fun onResume() {
-        super.onResume()
-        if (!bluetoothAdapter.isEnabled) {
-            promptEnableBluetooth()
-        }
-
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun promptEnableBluetooth() {
-        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            ENABLE_BLUETOOTH_REQUEST_CODE -> {
-                if (resultCode != RESULT_OK) {
-                    promptEnableBluetooth()
-                }
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            LOCATION_PERMISSION_REQUEST_CODE -> {
-                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
-                    requestLocationPermission()
-                } else {
-                    startBLEScan()
-                }
-            }
-        }
-    }
-
-    // CALLBACK BODIES
-
-    private val scanCallback = object : ScanCallback() {
-        @SuppressLint("MissingPermission")
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            // this might needs to be changed as the device.address might change due to
-            // MAC randomization
-            // check if the current found result is already in the entire scanResult list
-
-            val manufacturerId = result.scanRecord?.manufacturerSpecificData
-            val device = DeviceFactory.createDevice(manufacturerId)
-            device.printManufacturer()
-            val indexQuery = scanResults.indexOfFirst { it.device.address == result.device.address }
-            // element not found returns -1
-            if (indexQuery != -1) { // A scan result already exists with the same address
-                scanResults[indexQuery] = result
-                scanResultAdapter.notifyItemChanged(indexQuery)
-            } else { // found new device
-                with(result.device) {
-                    Log.i("ScanCallback", "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address")
-                }
-                scanResults.add(result)
-                scanResultAdapter.notifyItemInserted(scanResults.size - 1)
-            }
-        }
-
-        override fun onScanFailed(errorCode: Int) {
-            Log.e("ScanCallback", "onScanFailed: code $errorCode")
-        }
-    }
-
-    // PRIVATE FUNCTIONS
-
-    private fun Context.hasPermission(permissionType: String): Boolean {
-        return ContextCompat.checkSelfPermission(this, permissionType) ==
-                PackageManager.PERMISSION_GRANTED
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun startBLEScan() {
-        if (!isLocationPermissionGranted) {
-            requestLocationPermission()
-        } else {
-            scanResults.clear()
-            scanResultAdapter.notifyDataSetChanged()
-            bleScanner.startScan(null, scanSettings, scanCallback)
-            isScanning = true
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun stopBleScan() {
-        bleScanner.stopScan(scanCallback)
-        isScanning = false
-    }
-
-    private fun requestLocationPermission() {
-        if (isLocationPermissionGranted) {
-            return
-        }
-        MaterialAlertDialogBuilder(this)
-            .setTitle("Location Permission required")
-            .setMessage("Starting from Android M (6.0), the system requires apps to be granted " +
-                "location access in order to scan for BLE devices.")
-            .setPositiveButton("Ok") { dialog, which ->
-                requestPermission(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
-            }
-            .show()
-    }
-
-    private fun Activity.requestPermission(permission: String, requestCode: Int) {
-        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-    }
-
-    private fun setupRecyclerView() {
-        binding.scanResultsRecyclerView.apply {
-            adapter = scanResultAdapter
-            layoutManager = LinearLayoutManager(
-                this@MainActivity,
-                RecyclerView.VERTICAL,
-                false
-            )
-            isNestedScrollingEnabled = false
-        }
-
-        val animator = binding.scanResultsRecyclerView.itemAnimator
-        if (animator is SimpleItemAnimator) {
-            animator.supportsChangeAnimations = false
-        }
     }
 
 }
