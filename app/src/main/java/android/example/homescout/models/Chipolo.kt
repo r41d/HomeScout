19dcbb5e9f6a9c930954342827b4f7cb4d826aa0
LouisBienz <louis.bienz@gmail.com>
2022-10-18 10:00:08 +0200

    create class for chipolo devices

---
+++ app/src/main/java/android/example/homescout/models/Chipolo.kt
@@ -0,0 +1,10 @@
+package android.example.homescout.models
+
+import android.util.Log
+
+class Chipolo : Device {
+
+    override fun printManufacturer() {
+        Log.i("DeviceType", "Chipolo")
+    }
+}
