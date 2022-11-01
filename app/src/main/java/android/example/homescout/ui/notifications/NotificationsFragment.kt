aeb639ba21d549390da07f1d61ddb8f4db78bd42
LouisBienz <louis.bienz@gmail.com>
2022-11-01 10:38:07 +0100

    navigate to NotificationsFragment.kt when clicking on the notification

--- app/src/main/java/android/example/homescout/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/android/example/homescout/ui/notifications/NotificationsFragment.kt
@@ -1,5 +1,8 @@
 package android.example.homescout.ui.notifications
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.example.homescout.MainActivity
 import android.example.homescout.R
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,6 +14,11 @@ import androidx.lifecycle.ViewModelProvider
 import android.example.homescout.databinding.FragmentNotificationsBinding
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavDeepLinkBuilder
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 
 private const val CHANNEL_ID = "1"
 
@@ -43,16 +51,30 @@ class NotificationsFragment : Fragment() {
     }
 
     private fun createAndSendNotification() {
+
+        val intent = Intent(requireContext(), MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = NavDeepLinkBuilder(requireContext())
+            .setComponentName(MainActivity::class.java)
+            .setGraph(R.navigation.mobile_navigation)
+            .setDestination(R.id.navigation_notifications)
+            .createPendingIntent()
+
+
         var builder = NotificationCompat.Builder(requireContext(), CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notifications_24px)
             .setContentTitle("Hello")
             .setContentText("This is my first notification")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(requireContext())) {
             notify(1, builder.build())
         }
+
     }
 
     override fun onDestroyView() {
